6d7de8500f35355b5a4e29a28f2092cbdbda8e94
unknown <marko.rodriguez@hotmail.es>
2022-08-09 13:55:43 -0500

    get route added

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, {Request, Response} from "express";
 import cors from "cors"
 import productsRouter from "./routes/productsRouter";
 import categoryRouter from "./routes/categoryRouter";
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (_req:Request, res:Response)=>{
+  res.send('<p>Código disponible acá: <a href="https://github.com/markorodriguez/bsale-backend">github repo</a></p>')
+})
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server running");
